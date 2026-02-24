dc9b723c887f3c830b76ef5ddc8b34bdacec747e
Sagar Dhankar <sag9315@gmail.com>
2026-02-24 15:18:01 +0530

    React ReactDom is now from node modules import , not from CDN's . npm install react react-dom ; npx parcel index.html

--- React/1. Basic/index.js
+++ React/1. Basic/index.js
@@ -1,3 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 let heading = React.createElement("h1", {}, "Hello from React CDNs!")
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(heading)
