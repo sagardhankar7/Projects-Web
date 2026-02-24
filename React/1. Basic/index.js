ec8497e57ca9775187fc86cfdb3f923e81959c22
Sagar Dhankar <sag9315@gmail.com>
2026-02-24 15:31:56 +0530

    Created nested div structure using React.createElement()

--- React/1. Basic/index.js
+++ React/1. Basic/index.js
@@ -1,6 +1,12 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-let heading = React.createElement("h1", {}, "Hello from React CDNs!")
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+let heading = React.createElement(
+  "div", { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {key: "h1"}, "This is h1 tag"),
+    React.createElement("h2", {key: "h2"}, "This is h2 tag"),
+  ]),
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
