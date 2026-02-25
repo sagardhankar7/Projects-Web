7228d0c2067af1f0c2f17056cf0ddcaa46b25891
Sagar Dhankar <sag9315@gmail.com>
2026-02-25 08:58:59 +0530

    Functional Component inside Functional Component Called Functional Composition

--- React/1. Basic/index.js
+++ React/1. Basic/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 
 const heading = <h1>This is JXS (Behind the scene- React Element) Babel convert the Jsx to react element</h1>
 
+const Component2 = () => (<h1>
+    This is for Functional Composition, One functional component inside another component
+</h1>)
+
 const HeadingComponent = () => 
 (
 <div>Functional Component returns the jsx
-
+    <Component2/>
     <div>This is another div</div> 
     {heading}
 </div>
