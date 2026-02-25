9f06d6e76674d65b7755da73065860d700652af0
Sagar Dhankar <sag9315@gmail.com>
2026-02-25 08:54:07 +0530

    Functional Component

--- React/1. Basic/index.js
+++ React/1. Basic/index.js
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom/client";
 
 const heading = <h1>This is JXS (Behind the scene- React Element) Babel convert the Jsx to react element</h1>
 
-const HeadingComponent = () => <div>Functional Component returns the jsx</div>
+const HeadingComponent = () => 
+(
+<div>Functional Component returns the jsx
+
+    <div>This is another div</div>
+</div>
+
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<HeadingComponent/>);
