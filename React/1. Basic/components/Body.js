91bb3e897a1e691d48ab882f327b99be6c382c72
Sagar Dhankar <sag9315@gmail.com>
2026-02-28 10:31:39 +0530

    Search Filter the content

--- React/1. Basic/components/Body.js
+++ React/1. Basic/components/Body.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer"
 // import { resList } from "./DummyData";
 
 const Body = () => {
     const [stateList, setStateList] = useState([]);
-    // const [stateFilteredList, setStateFilteredList] = useState
+    const [searchText, setSearchText] = useState("")
+    const [filteredRest, setFilteredRest] = useState([])
     useEffect(()=>{
         fetchData();
     },[])
@@ -13,20 +15,31 @@ const Body = () => {
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.3102094&lng=77.3698382&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
 
-        const restaurants = json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-        console.log(restaurants);
+        const restaurants = json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        // console.log(restaurants);
         setStateList(restaurants)
+        setFilteredRest(restaurants)
     }
     
+    if(stateList.length == 0) return <Shimmer></Shimmer>
+
     return (
         <div className="main-body">
             <div className="search-container">
-                <input type="text"></input>
-                <button>Search</button>
+                <input type="text" value={searchText} onChange={(e)=>{
+                    setSearchText(e.target.value)
+                    console.log(searchText,"31")
+                }}></input>
+                <button onClick={()=>{
+                    // console.log(filteredRest[0].info.name)
+                    const filteredList = stateList.filter((rest)=> rest.info.name.toLowerCase().includes(searchText.toLowerCase()))
+                    // Whenever the State Variable Updates. React ReRender the UIComponent.
+                    setFilteredRest(filteredList)
+                }}>Search</button>
                 <button onClick={()=>{
                     const filteredList = stateList.filter((restaurant)=>restaurant.info.avgRating>4.0)
                     // Whenever the State Variable Updates. React ReRender the UIComponent.
-                    setStateList(filteredList)
+                    setFilteredRest(filteredList)
                 }}>
                     Top Rated Restaurant
                 </button>
@@ -34,8 +47,8 @@ const Body = () => {
 
             <div className="restaurant-container">
                 {/* PROPS Structure is better */}
-                {console.log(stateList)}
-                {stateList.map((restaurant)=>(
+                {console.log(filteredRest, "50")}
+                {filteredRest.map((restaurant)=>(
                     <RestaurantCard key={restaurant.info.id} resData={restaurant} />
                 ))}
             </div>
